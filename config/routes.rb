043deace6943b11bc9f136dae4ce0c1d42d79466
unknown <migs432@yahoo.com>
2015-03-07 00:55:17 -0800

    sign-up-in-n-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+
+#  get 'welcome/index'
+
+#  get 'welcome/about'
+  
+  get 'about' => 'welcome#about'
+  
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
